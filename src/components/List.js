ed33ee39006161ef1cdccde3a038699244f6bacf
gagarin525125 <gagarin525125@gmail.com>
2016-11-23 13:18:15 +0100

    update 23.11 noon

--- src/components/List.js
+++ src/components/List.js
@@ -4,25 +4,32 @@ import React, { PropTypes } from 'react';
  * A stateless function component
  * https://facebook.github.io/react/docs/reusable-components.html#stateless-functions
  */
-export default function List({ items = [], onItemClick,onLevelDownClick }) {
+export default function List({ items = [], onItemClick,onLevelDownClick ,onShowMapClick,levels}) {
     // Create a list of li elements that make up the list of organisation units units
     // Each has a click handler for that specific item
+    console.log("List");
+    console.log({items}
+
+    );
     const listItems = items
         .map(item => {
+            if(item.level === levels ){
             return (
 
-                     <li key={item.id} >
-                         {item.displayName}
-                         <button onClick={() => onItemClick(item)}> info </button>
-                         <button onClick={() => onLevelDownClick(item)}> drill down </button>
-                     </li>
+                            <li key={item.id} >
+                                  {item.displayName}
+                                  <button onClick={() => onItemClick(item)}> info </button>
+                                  < button onClick={() => onLevelDownClick(item)}> drill down </button>
+                            </li>
 
-
-
-
-
-
-            );
+            );} else {
+                return(
+                    <li key={item.id} >
+                        {item.displayName}
+                        <button onClick={() => onItemClick(item)}> info </button>
+                        < button onClick = {() => onShowMapClick(item)}>show map</button>
+                    </li>
+                        ) }
         });
 
     // Render the list with the items
