141d83d370716572bbd80b67cd9afcc0cbdfbd2a
gagarin525125 <gagarin525125@gmail.com>
2016-11-25 21:47:04 +0100

    update 25.11  late

--- src/components/List.js
+++ src/components/List.js
@@ -4,17 +4,32 @@ import React, { PropTypes } from 'react';
  * A stateless function component
  * https://facebook.github.io/react/docs/reusable-components.html#stateless-functions
  */
-export default function List({ items = [], onItemClick,onLevelDownClick ,onDeleteClick,levels}) {
+export default function List({ items = [], onItemClick,onLevelDownClick ,onSelectClick,levels}) {
     // Create a list of li elements that make up the list of organisation units units
     // Each has a click handler for that specific item
+
+
+
+    //   this doesnt work
+    // let bol =  items.every((val,i,arr) => val == arr[0].parent.id);
+       let bol = true;
+      for(let i = 0;i < items.length - 1;i++){
+          if(items[i].parent.id === items[i+1].parent.id){
+              bol = bol && true;
+          }else {
+              bol = bol && false;
+          }
+
+      }
 
     const listItems = items
         .map(item => {
             if(item.level === levels ){
                 return(
-                    <li key={item.id} onClick={() => onItemClick(item)}>
+                    <li key={item.id} >
                         {item.displayName}
-                        <button className="delete_button" onClick={() => onDeleteClick(item)}>Delete</button>
+                        <button   onClick={() => onItemClick(item)}>info</button>
+                        <button className="select_button" disabled={bol} onClick={() => onSelectClick(item)}>home</button>
 
                     </li>
                 ) } else {
@@ -43,6 +58,8 @@ List.propTypes = {
     items: PropTypes.array,
     onItemClick: PropTypes.func.isRequired,
     onLevelDownClick: PropTypes.func.isRequired,
+    onSelectClick: PropTypes.func.isRequired,
+    levels: PropTypes.number,
 
 };
 
