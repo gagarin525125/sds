78457aa34bf88fa5a765ff688621c45df07d6826
torhus <torhu@yahoo.com>
2016-11-29 19:28:08 +0100

    Trenger ikke state i MaxResults

--- src/components/MaxResults.js
+++ src/components/MaxResults.js
@@ -8,19 +8,17 @@ import React from 'react';
 export default class MaxResults extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: props.value};
 
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
         this.props.onChange(event.target.value);
     }
 
     render() {
         return (
-            <select value={this.state.value} onChange={this.handleChange}>
+            <select value={this.props.value} onChange={this.handleChange}>
                 <option value="10">10 results</option>
                 <option value="20">20 results</option>
                 <option value="30">30 results</option>
