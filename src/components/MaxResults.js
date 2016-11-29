30823a5e0cc5f26b7d2f6d23e0b680adf7a26eeb
torhus <torhu@yahoo.com>
2016-11-29 19:34:01 +0100

    Forenkle MaxResults end mer

--- src/components/MaxResults.js
+++ src/components/MaxResults.js
@@ -1,24 +1,17 @@
 /**
  * Pulldown list to set the maximum number of search results.
  */
-
 import React from 'react';
-
 
 export default class MaxResults extends React.Component {
     constructor(props) {
         super(props);
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event) {
-        this.props.onChange(event.target.value);
     }
 
     render() {
         return (
-            <select value={this.props.value} onChange={this.handleChange}>
+            <select value={this.props.value}
+                    onChange={ev => this.props.onChange(ev.target.value)}>
                 <option value="10">10 results</option>
                 <option value="20">20 results</option>
                 <option value="30">30 results</option>
